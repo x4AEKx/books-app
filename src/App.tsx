41205752cfaf4a55d88d878a5c26d9bbbed6c6e3
Dmitry '4aek' Vlasov <4aek20@gmail.com>
2021-05-15 12:46:22 +0300

    test version of the book application

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,108 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {ChangeEvent, useState} from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		const [inputValue, setInputValue] = useState("")
+
+		const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+				setInputValue(e.currentTarget.value)
+		}
+
+		return (
+				<div className="App">
+						<div style={{marginTop: "10px"}}>
+								<input type="text" value={inputValue} onChange={handleOnChange}/>
+								<button style={{marginLeft: "5px"}}>search</button>
+						</div>
+						<div style={{
+								display: "flex",
+								justifyContent: "space-around",
+								alignItems: "center",
+								flexWrap: "wrap",
+								marginTop: "10px"
+						}}>
+								<div style={{
+										display: "flex",
+										flexDirection: "column",
+										alignItems: "center",
+										width: "33.3333%",
+										outline: "1px solid black"
+								}}>
+										<div style={{width: "100px", height: "100px", backgroundColor: "black", marginTop: "10px"}}></div>
+										<h3>book name</h3>
+										<p>book`s author</p>
+								</div>
+								<div style={{
+										display: "flex",
+										flexDirection: "column",
+										alignItems: "center",
+										width: "33.3333%",
+										outline: "1px solid black"
+								}}>
+										<div style={{width: "100px", height: "100px", backgroundColor: "black", marginTop: "10px"}}></div>
+										<h3>book name</h3>
+										<p>book`s author</p>
+								</div>
+								<div style={{
+										display: "flex",
+										flexDirection: "column",
+										alignItems: "center",
+										width: "33.3333%",
+										outline: "1px solid black"
+								}}>
+										<div style={{width: "100px", height: "100px", backgroundColor: "black", marginTop: "10px"}}></div>
+										<h3>book name</h3>
+										<p>book`s author</p>
+								</div>
+								<div style={{
+										display: "flex",
+										flexDirection: "column",
+										alignItems: "center",
+										width: "33.3333%",
+										outline: "1px solid black"
+								}}>
+										<div style={{width: "100px", height: "100px", backgroundColor: "black", marginTop: "10px"}}></div>
+										<h3>book name</h3>
+										<p>book`s author</p>
+								</div>
+								<div style={{
+										display: "flex",
+										flexDirection: "column",
+										alignItems: "center",
+										width: "33.3333%",
+										outline: "1px solid black"
+								}}>
+										<div style={{width: "100px", height: "100px", backgroundColor: "black", marginTop: "10px"}}></div>
+										<h3>book name</h3>
+										<p>book`s author</p>
+								</div>
+								<div style={{
+										display: "flex",
+										flexDirection: "column",
+										alignItems: "center",
+										width: "33.3333%",
+										outline: "1px solid black"
+								}}>
+										<div style={{width: "100px", height: "100px", backgroundColor: "black", marginTop: "10px"}}></div>
+										<h3>book name</h3>
+										<p>book`s author</p>
+								</div>
+								<div style={{
+										display: "flex",
+										flexDirection: "column",
+										alignItems: "center",
+										width: "33.3333%",
+										outline: "1px solid black"
+								}}>
+										<div style={{width: "100px", height: "100px", backgroundColor: "black", marginTop: "10px"}}></div>
+										<h3>book name</h3>
+										<p>book`s author</p>
+								</div>
+						</div>
+
+
+				</div>
+		);
 }
 
 export default App;

23e4dda74177208a6b6c5d4986cebaba9f490878
Dmitry '4aek' Vlasov <4aek20@gmail.com>
2021-05-16 20:40:09 +0300

    test version: created Modal component

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,58 @@ import "./App.css";
 import axios from "axios";
 import {bookType} from "./type/book";
 
+type ModalPropsType =
+		{
+				authors: Array<string> | null
+				cover: null | number
+				visible: boolean
+				title: null | string
+				onClose: () => void
+		}
+
+const Modal = React.memo(function ({
+																			 cover,
+																			 authors,
+																			 visible = false,
+																			 title,
+																			 onClose,
+																	 }: ModalPropsType) {
+
+		if (!visible) return null
+
+		return (
+				<div className="modal" onClick={onClose}>
+						<div className="modal-dialog" onClick={e => e.stopPropagation()}>
+								<div className="modal-header">
+										<h3 className="modal-title">{title}</h3>
+										<span className="modal-close" onClick={onClose}>
+            &times;
+          </span>
+								</div>
+								<div className="modal-body">
+										<div className="modal-content">
+												<div style={{
+														width: "100%",
+														height: "370px",
+														marginTop: "10px",
+														background: `url(https://covers.openlibrary.org/b/id/${cover}-L.jpg) center / contain no-repeat`
+												}}/>
+												{authors && authors.map(author => <div>{author}</div>)}
+										</div>
+								</div>
+						</div>
+				</div>
+		)
+})
+
 function App() {
 		const [inputValue, setInputValue] = useState("")
 
 		const [books, setBooks] = useState<Array<bookType>>([])
+
+		const [cover, setCover] = useState<number | null>(null)
+		const [title, setTitle] = useState<string | null>(null)
+		const [authors, setAuthors] = useState<Array<string> | null>(null)
 
 		const getBooks = async (value: string) => {
 				const {data} = await axios.get(`https://openlibrary.org/search.json?title=${value}`)
@@ -18,10 +66,13 @@ function App() {
 				const timeout = setTimeout(async () => {
 						const data = await getBooks(inputValue)
 						setBooks(data)
+						console.log("timeout start")
 				}, 1000)
+
 
 				return () => {
 						clearTimeout(timeout)
+						console.log("timeout break")
 				}
 		}, [inputValue])
 
@@ -31,12 +82,24 @@ function App() {
 				setInputValue(e.currentTarget.value)
 		}
 
+		const [isModal, setModal] = useState(false)
+		const onClose = () => setModal(false)
+
 		return (
 				<div className="App">
 						<div style={{marginTop: "10px"}}>
 								<input type="text" value={inputValue} onChange={handleOnChange}/>
 								<button style={{marginLeft: "5px"}}>search</button>
 						</div>
+
+						<Modal
+								visible={isModal}
+								title={title}
+								cover={cover}
+								authors={authors}
+								onClose={onClose}
+						/>
+
 						<div style={{
 								display: "flex",
 								justifyContent: "space-around",
@@ -45,20 +108,27 @@ function App() {
 								marginTop: "10px"
 						}}>
 								{books.map(book => (
-												<div key={book.key} style={{
+												<div onClick={() => {
+														setCover(book.cover_i)
+														setTitle(book.title)
+														setModal(true)
+														setAuthors(book.author_name)
+												}} key={book.key} style={{
 														display: "flex",
 														flexDirection: "column",
 														alignItems: "center",
 														justifyContent: "space-between",
 														width: "33.3333%",
-														outline: "1px solid black"
+														outline: "1px solid black",
+														cursor: "pointer"
 												}}>
+
 														{book.cover_i ? <div style={{
 																width: "100%",
 																height: "280px",
 																marginTop: "10px",
 																background: `url(https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg) center / contain no-repeat`
-														}}></div> : <div style={{
+														}}/> : <div style={{
 																width: "100%",
 																height: "280px",
 																marginTop: "10px",
@@ -66,7 +136,11 @@ function App() {
 																color: "white"
 														}}>No Image</div>}
 														<h3>{book.title}</h3>
-														<p>author: {book.author_name}</p>
+														{book.author_name && book.author_name.length > 0
+																? (<p>Author: {book.author_name.map(name => <span>{name}</span>)}</p>)
+																: (<p>Author: -</p>)
+														}
+
 												</div>
 										)
 								)}
@@ -74,5 +148,6 @@ function App() {
 				</div>
 		);
 }
+
 
 export default App;
